3bc7aae71c0fd61095aa5ac7ded9084407a9b702
Adophilus <uchenna19of@gmail.com>
2024-01-26 02:50:54 +0000

    fixed some type errors

--- src/routes/mail/-components/mail.tsx
+++ src/routes/mail/-components/mail.tsx
@@ -71,10 +71,10 @@ export function Mail({
           collapsible={true}
           minSize={15}
           maxSize={20}
-          onCollapse={(collapsed) => {
-            setIsCollapsed(collapsed)
+          onCollapse={() => {
+            setIsCollapsed(!isCollapsed)
             document.cookie = `react-resizable-panels:collapsed=${JSON.stringify(
-              collapsed
+              !isCollapsed
             )}`
           }}
           className={cn(isCollapsed && "min-w-[50px] transition-all duration-300 ease-in-out")}

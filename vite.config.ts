f70aac1b3b96af05a1a8108571a983d4c3724fd6
Austin Chris <austinchrisiwu@gmail.com>
2024-01-26 03:53:50 +0100

    Revert "Screen scaffold"

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
-import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
-import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     nodePolyfills(),
-    TanStackRouterVite(),
     react()
   ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 })

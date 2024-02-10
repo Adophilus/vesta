76148d9b1af45617d4501dd20870eadc65e1b846
Adophilus <uchenna19of@gmail.com>
2024-02-10 20:00:31 +0000

    updated the sidebar

--- src/routes/_mail/mail/-components/sidebar.tsx
+++ src/routes/_mail/mail/-components/sidebar.tsx
@@ -1,27 +1,11 @@
 import { Separator } from "@/components/shad/ui/separator";
-import { AlertCircleIcon, ArchiveIcon, ArchiveXIcon, FileIcon, InboxIcon, MessagesSquareIcon, SendIcon, ShoppingCartIcon, Trash2Icon, Users2Icon } from "lucide-react";
+import { AlertCircleIcon, ArchiveIcon, ArchiveXIcon, FileIcon, InboxIcon, LucideIcon, MessagesSquareIcon, SendIcon, ShoppingCartIcon, Trash2Icon, Users2Icon } from "lucide-react";
 import { Nav } from "./nav";
 import { MailCompose } from "./mail-compose";
 import { AccountSwitcher } from "./account-switcher";
-import { accounts } from "./data";
-import { useState } from "react";
 import { cn } from "@/lib/shad/utils";
-import { useGetMailsReceived } from "./hooks/mail";
-import MailInterface from "@/lib/interfaces/mail";
-import { groupBy } from "lodash/groupBy";
 
-function countMailFolder(mails?: MailInterface.MailReceived.Fetch[]) {
-  if (mails) {
-    const groups = groupBy(mails, "folder")
-    console.log(groups)
-  }
-}
-
-export function Sidebar({ isCollapsed, setIsCollapsed }: { isCollapsed: boolean, setIsCollapsed: (_: boolean) => void }) {
-  const { data } = useGetMailsReceived()
-
-  const count = countMailFolder(data)
-
+export function Sidebar({ isCollapsed }: { isCollapsed: boolean, setIsCollapsed: (_: boolean) => void }) {
   return (
     <>
       <div className={cn("flex h-[52px] items-center justify-center", isCollapsed ? 'h-[52px]' : 'px-2')}>
@@ -32,47 +16,9 @@ export function Sidebar({ isCollapsed, setIsCollapsed }: { isCollapsed: boolean,
       <Separator />
       <Nav
         isCollapsed={isCollapsed}
-        links={[
-          {
-            title: "Inbox",
-            label: count.inbox,
-            icon: InboxIcon,
-            variant: "default",
-          },
-          {
-            title: "Drafts",
-            label: count.drafts,
-            icon: FileIcon,
-            variant: "ghost",
-          },
-          {
-            title: "Sent",
-            label: count.sent,
-            icon: SendIcon,
-            variant: "ghost",
-          },
-          {
-            title: "Junk",
-            label: count.junk,
-            icon: ArchiveXIcon,
-            variant: "ghost",
-          },
-          {
-            title: "Trash",
-            label: count.trash,
-            icon: Trash2Icon,
-            variant: "ghost",
-          },
-          {
-            title: "Archive",
-            label: count.archive,
-            icon: ArchiveIcon,
-            variant: "ghost",
-          },
-        ]}
       />
       <Separator />
-      <Nav
+      {/*<Nav
         isCollapsed={isCollapsed}
         links={[
           {
@@ -106,7 +52,7 @@ export function Sidebar({ isCollapsed, setIsCollapsed }: { isCollapsed: boolean,
             variant: "ghost",
           },
         ]}
-      />
+      />*/}
     </>
   )
 }

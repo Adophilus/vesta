82fdd4f7a27d319b01d39c60d95fbb6181e95dd9
Adophilus <uchenna19of@gmail.com>
2024-02-11 20:42:19 +0000

    added a new method to get sent mails

--- src/lib/services/mail.ts
+++ src/lib/services/mail.ts
@@ -17,6 +17,27 @@ namespace MailService {
     return mail
   }
 
+  type GetSentMailsPayload = {
+    profile: SerializedJunoDoc<UserProfileInterface.UserProfile.Fetch>
+  }
+
+  export const getSentMails = async ({ profile }: GetSentMailsPayload): Promise<MailInterface.MailReceived.Fetch[]> => {
+    const docs: ListResults<MailInterface.MailReceived.Fetch> = await listDocs({
+      collection: MAILS_SENT_COLLECTION_KEY,
+      filter: {
+        matcher: {
+          description: `<|senderEmail:${profile.data.email}|>`
+        },
+        order: {
+          desc: true,
+          field: "created_at",
+        },
+      },
+    });
+
+    return docs.items
+  }
+
   type GetReceivedMailsPayload = {
     profile: SerializedJunoDoc<UserProfileInterface.UserProfile.Fetch>
   }
@@ -54,6 +75,7 @@ namespace MailService {
       collection: MAILS_SENT_COLLECTION_KEY,
       doc: {
         key: ulid(),
+        description: `<|senderEmail:${payload.sender.email}|>`,
         data: {
           ...payload,
           sentAt: new Date()
@@ -61,7 +83,7 @@ namespace MailService {
       },
     });
 
-    for (const recipient of [payload.recipientEmail, ...payload.cc, ...payload.bcc]) {
+    for (const recipient of [payload.recipientEmail, payload.sender.email, ...payload.cc, ...payload.bcc]) {
       await setDoc<MailInterface.MailReceived.Create>({
         collection: MAILS_RECEIVED_COLLECTION_KEY,
         doc: {

c9cd1820f483d79283a8d994e42d43deb5e9b99b
Adophilus <uchenna19of@gmail.com>
2024-02-11 22:15:11 +0000

    config

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,21 @@ import react from '@vitejs/plugin-react-swc'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 import path from "path"
-// import juno from "@junobuild/vite-plugin";
+import { Crypto } from "@peculiar/webcrypto"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     nodePolyfills(),
-    // juno(),
     TanStackRouterVite(),
     react(),
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src")
+      "@": path.resolve(__dirname, "./src"),
+      global: {
+        crpto: new Crypto()
+      }
     },
   }
 })

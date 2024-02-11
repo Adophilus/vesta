24f9c26484d97634f05f3aa6e70687d250ac37a5
Adophilus <uchenna19of@gmail.com>
2024-02-11 22:14:32 +0000

    reverting...

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 import path from "path"
-import juno from "@junobuild/vite-plugin";
+// import juno from "@junobuild/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     nodePolyfills(),
-    juno(),
+    // juno(),
     TanStackRouterVite(),
     react(),
   ],

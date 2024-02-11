3591fc21aa13e754760158bd74de61567017e972
Adophilus <uchenna19of@gmail.com>
2024-02-11 17:23:18 +0000

    successful build this time

--- src/app.tsx
+++ src/app.tsx
@@ -17,15 +17,18 @@ declare module '@tanstack/react-router' {
   }
 }
 
-await AuthService.init()
 
-// Render the app
-const rootElement = document.getElementById('root')!
-if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement)
-  root.render(
-    <StrictMode>
-      <RouterProvider router={router} />
-    </StrictMode>,
-  )
-}
+(async () => {
+  await AuthService.init()
+
+  // Render the app
+  const rootElement = document.getElementById('root')!
+  if (!rootElement.innerHTML) {
+    const root = ReactDOM.createRoot(rootElement)
+    root.render(
+      <StrictMode>
+        <RouterProvider router={router} />
+      </StrictMode>,
+    )
+  }
+})()

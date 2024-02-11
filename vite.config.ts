ba3b1f0ae3638bd99a5c3a52030dabb8a0680b7b
Adophilus <uchenna19of@gmail.com>
2024-02-11 22:54:53 +0000

    checkpointing...

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,8 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    nodePolyfills(),
     TanStackRouterVite(),
+    nodePolyfills(),
     react(),
   ],
   resolve: {

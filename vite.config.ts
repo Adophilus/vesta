f892903c60b997bc4da277257b2805e71a670e0d
Adophilus <uchenna19of@gmail.com>
2024-02-12 02:14:08 +0000

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
-import { Crypto } from "@peculiar/webcrypto"
 import path from "path"
-
-const crypto = new Crypto()
 
 export default defineConfig({
   plugins: [

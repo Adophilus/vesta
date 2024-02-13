cd703e00acac25e69bcbfbe2106e6441972f916f
Adophilus <uchenna19of@gmail.com>
2024-02-13 00:34:50 +0000

    updated the hero

--- src/routes/-components/hero.tsx
+++ src/routes/-components/hero.tsx
@@ -1,6 +1,7 @@
 import { ArrowRightIcon, RocketIcon, SparkleIcon, SparklesIcon } from "lucide-react";
 import { Button } from "./button";
 import { Link } from "@tanstack/react-router";
+import { motion } from "framer-motion"
 import "./hero.css"
 import "./navbar.css"
 
@@ -12,12 +13,8 @@ export function Hero() {
       <div
         className="backed grow flex flex-col gap-12 items-center justify-center"
       >
-        <div className="flex flex-col gap-y-4">
-          <Link
-            to="/workspace"
-            className="border-2 self-start bg-primary font-semibold text-xs border-black rounded-full p-1 px-2 pr-4 inline-flex gap-2 items-center select-none">
-            <span className="rounded-full bg-black text-white p-2 px-4 inline-flex items-center gap-1"><SparklesIcon className="fill-white w-3 h-3" /> NEW</span> We're working on some new amazing featured <ArrowRightIcon className="w-4 h-4 stroke-[3px] fill-black stroke-black" />
-          </Link>
+        <div className="flex flex-col gap-y-4 p-4">
+          <WhatsNew />
           <header
             className="font-Poppins font-bold text-center text-5xl leading-relaxed lg:leding-noraml lg:text-8xl max-w-2xl lg:max-w-6xl px-4 lg:px-8"
           >
@@ -47,5 +44,30 @@ export function Hero() {
         </Link>
       </div>
     </section>
+  )
+}
+
+function WhatsNew() {
+  return (
+    <Link
+      to="/workspace"
+    >
+      <motion.button
+        className="border-2 self-start bg-primary font-semibold text-[10px] md:text-xs border-black rounded-full p-1 px-2 pr-4 inline-flex gap-2 items-center select-none shadow-[6px_4px_0px_1px_rgb(0,0,0)] focus:outline-none"
+        whileTap={{
+          boxShadow: "0px 0px 0px 0px",
+          backgroundColor: "#ffffff",
+          color: "rgb(0, 0, 0)",
+          x: 6,
+          y: 4,
+          transition: {
+            ease: "linear",
+            duration: 0.25,
+          }
+        }}
+      >
+        <span className="rounded-full bg-black text-white p-2 px-4 inline-flex items-center gap-1"><SparklesIcon className="fill-white w-3 h-3" /> BETA</span> Have a sneak-peak at our new features <ArrowRightIcon className="w-4 h-4 stroke-[3px] fill-black stroke-black" />
+      </motion.button>
+    </Link>
   )
 }

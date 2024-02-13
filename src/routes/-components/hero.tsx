734143a23ce475217099d8d6d905803a5d7ebbc6
Adophilus <uchenna19of@gmail.com>
2024-02-13 00:24:46 +0000

    updated the home page

--- src/routes/-components/hero.tsx
+++ src/routes/-components/hero.tsx
@@ -1,4 +1,4 @@
-import { ArrowRightIcon, RocketIcon } from "lucide-react";
+import { ArrowRightIcon, RocketIcon, SparkleIcon, SparklesIcon } from "lucide-react";
 import { Button } from "./button";
 import { Link } from "@tanstack/react-router";
 import "./hero.css"
@@ -12,21 +12,28 @@ export function Hero() {
       <div
         className="backed grow flex flex-col gap-12 items-center justify-center"
       >
-        <header
-          className="font-Poppins font-bold text-center text-5xl leading-relaxed lg:leding-noraml lg:text-8xl max-w-2xl lg:max-w-6xl px-4 lg:px-8"
-        >
-          Increase <span
-            style={{ WebkitTextStroke: "3px black" }}
-            className="text-primary"
+        <div className="flex flex-col gap-y-4">
+          <Link
+            to="/workspace"
+            className="border-2 self-start bg-primary font-semibold text-xs border-black rounded-full p-1 px-2 pr-4 inline-flex gap-2 items-center select-none">
+            <span className="rounded-full bg-black text-white p-2 px-4 inline-flex items-center gap-1"><SparklesIcon className="fill-white w-3 h-3" /> NEW</span> We're working on some new amazing featured <ArrowRightIcon className="w-4 h-4 stroke-[3px] fill-black stroke-black" />
+          </Link>
+          <header
+            className="font-Poppins font-bold text-center text-5xl leading-relaxed lg:leding-noraml lg:text-8xl max-w-2xl lg:max-w-6xl px-4 lg:px-8"
           >
-            productivity
-          </span> with our all-in-one <span
-            style={{ WebkitTextStroke: "3px black" }}
-            className="text-primary"
-          >
-            Platform
-          </span>
-        </header>
+            Increase <span
+              style={{ WebkitTextStroke: "3px black" }}
+              className="text-primary"
+            >
+              productivity
+            </span> with our all-in-one <span
+              style={{ WebkitTextStroke: "3px black" }}
+              className="text-primary"
+            >
+              Platform
+            </span>
+          </header>
+        </div>
         <Link
           to="/mail/$mailFolder"
           params={{
